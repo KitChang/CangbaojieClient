ef549872d4d101cab5081f09f24eeceee1e8414a
MaximityIT <it.dept@ibeacon-macau.com>
2016-01-07 01:36:20 +0800

    add topup and message validation

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -15,7 +15,6 @@ module.exports = {
         
     },
     create: function(req, res){
-        
         var Session = require("../lib/session");
         var session = new Session(req.session);
         var user = session.user();
@@ -26,6 +25,10 @@ module.exports = {
         var email = req.param('email');
         var title = req.param('title');
         var message2 = req.param('message');
+        if (title=="" || name=="" || email=="" || contactPhone=="") {
+            res.redirect('/?message=NO');
+            return;
+        }
         message.create({user: user.id, client: clientId, name: name, contactPhone: contactPhone, email: email, title: title, message: message2}).exec(function(err){
             var messageStr = "用户"+user.username+"留言已发出-标题:"+title;
             ClientMessage.create({client: clientId, message: messageStr}).exec(function(){
--- api/controllers/TopupRequestController.js
+++ api/controllers/TopupRequestController.js
@@ -28,6 +28,10 @@ module.exports = {
         var contactPhone = req.param('contactPhone');
         var email = req.param('email');
         var remarks = req.param('remarks');
+        if (topupValue=="" || name=="" || email=="" || contactPhone=="") {
+            res.redirect('/?message=NO');
+            return;
+        }
         TopupRequest.create({user: user.id, client: clientId, topupValue: topupValue, name: name, contactPhone: contactPhone, email: email, remarks: remarks}).exec(function(err){
             var messageStr = "用户"+user.username+"充值请求已发车－充值:"+topupValue+"元";
             ClientMessage.create({client: clientId, message: messageStr}).exec(function(){
